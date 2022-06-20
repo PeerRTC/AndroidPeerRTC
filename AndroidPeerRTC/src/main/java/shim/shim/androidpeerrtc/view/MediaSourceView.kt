f372a77edab9f79adc0417533f037354209e55bb
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-06-20 12:39:25 +0800

    Clean spacing

--- AndroidPeerRTC/src/main/java/shim/shim/androidpeerrtc/view/MediaSourceView.kt
+++ AndroidPeerRTC/src/main/java/shim/shim/androidpeerrtc/view/MediaSourceView.kt
@@ -7,14 +7,15 @@ import shim.shim.androidpeerrtc.javascriptinterface.MediaConnectionJavascriptInt
 
 class MediaSourceView(context: Context, attr: AttributeSet?) :
     AndroidPeerInterfaceView(context, attr) {
-    companion object{
+    companion object {
         const val TYPE_AUDIO = 1
-        const val  TYPE_VIDEO = 2
-        const val  TYPE_AUDIO_VIDEO =3
+        const val TYPE_VIDEO = 2
+        const val TYPE_AUDIO_VIDEO = 3
 
         const val FRONT_CAM = 0
         const val BACK_CAM = 1
     }
+
     override val htmlUrl: String = "file:///android_asset/mediator/mediastream-source.html"
     override val TAG: String = "MediaSourceView"
 
